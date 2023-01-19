15d8e4424dbf9b1d539e33b7eaf9ea5603965269
kiriraksmeym <raksmeyvn98@gmail.com>
2023-01-19 16:53:09 +0700

    fix

--- src/main/java/com/metfone/topup/controller/PreorderController.java
+++ src/main/java/com/metfone/topup/controller/PreorderController.java
@@ -1,15 +1,18 @@
 package com.metfone.topup.controller;
 
 import com.metfone.topup.model.MobileDeposit;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+@Controller
 
 public class PreorderController {
     @GetMapping("/preorder")
-    public String createEmploye(Model model) {
+    public String preorder() {
         return "pre-order/index_pre-order";
     }
+
 }
