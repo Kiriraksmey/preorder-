4a1382bbef7756f465ff54af13297db99950e589
kiriraksmeym <raksmeyvn98@gmail.com>
2023-01-19 16:02:54 +0700

    first commit

--- src/main/java/com/metfone/topup/controller/PreorderController.java
+++ src/main/java/com/metfone/topup/controller/PreorderController.java
@@ -1,2 +1,15 @@
-package com.metfone.topup.controller;public class PreorderController {
+package com.metfone.topup.controller;
+
+import com.metfone.topup.model.MobileDeposit;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class PreorderController {
+    @GetMapping("/preorder")
+    public String createEmploye(Model model) {
+        return "pre-order/index_pre-order";
+    }
 }
